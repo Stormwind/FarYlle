d35acdf470aa2b732059fe6b1ddc1f09a4356a11
Stormwind <stormwind@stormwinds-page.de>
2011-07-23 22:20:04 +0200

    - add switch for debug logs - use own list for used log levels

--- logger/Logger.tcl
+++ logger/Logger.tcl
@@ -8,25 +8,51 @@ oo::class create Logger {
   # Constructor
   #
   constructor {} {
-    my variable level
-    set level "error"
-    
+    my variable logLevels
+    my setDebug 0
+    set logLevels [list "error" "notice" "info" "debug"]
   }
 
-  # Sets the Log level to the given value, if it exists
+  method getAllLogLevels {} {
+    my variable logLevels
+    return [join $logLevels " "]
+  }
+
+  # Returns state of the debug logger
   #
+  # @return Debug state
+  method getDebug {} {
+    my variable debug
+    return $debug
+  }
+
+  # Sets the debug logger on or off
   #
-  method setLogLevel {logLevel} {
-    my variable level
-    set logLevels [log::levels]
+  # @param logLevel The logLevel as string
+  # @return Debug state
+  method setDebug {debugSwitch} {
+    my variable debug
+    set debug $debugSwitch
+    return $debug
+  }
+
+  # Sets the log output to the given channel
+  #
+  # @param logLevel log level
+  # @param channel log channel
+  # @return 1
+  # @thows LOG BADLEVEL, if log level does not exist
+  method setLogChannel {logLevel channel} {
+    my variable logLevels
     if {[lsearch $logLevels $logLevel] == -1} {
       set message ""
-      append message "Log level '" $logLevel "' does not exist."
+      append message "Cannot set channel for log level '" $logLevel "', because this log level does not exist."
       throw {LOG BADLEVEL} $message
     } else {
-      set level $logLevel
       return 1
     }
   }
+
+
 
 }
